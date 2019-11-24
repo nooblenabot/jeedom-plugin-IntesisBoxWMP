0c392d6f8ed6e81e4633c23a508ce70b7a2ad07c
nooblenabot <benoit@granouille.ovh>
2019-11-24 10:16:55 +0100

    historize info values

--- desktop/js/IntesisBoxWMP.js
+++ desktop/js/IntesisBoxWMP.js
@@ -64,7 +64,11 @@ function addCmdToTable(_cmd) {
 				tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fa fa-cogs"></i></a> ';
 				tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
 				tr += '</select>';
-			tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> ';
+				tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked/>{{Afficher}}</label></span> ';
+				if(!isset(_cmd.type) || _cmd.type == 'info' )
+				{
+					tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" data-size="mini"/>{{Historiser}}</label></span> ';
+				}
 			}
 			
 			tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
